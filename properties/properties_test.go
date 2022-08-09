d0e34e786bd4680dd2537e45a8217e4483bf40da
Inhere <in.798@qq.com>
2022-08-09 23:41:28 +0800

    chore: add unit tests for properties driver

---
+++ properties/properties_test.go
@@ -0,0 +1,31 @@
+package properties_test
+
+import (
+	"testing"
+
+	"github.com/gookit/config/v2"
+	"github.com/gookit/config/v2/properties"
+	"github.com/gookit/goutil/testutil/assert"
+)
+
+func TestDriver(t *testing.T) {
+	is := assert.New(t)
+	is.Eq(properties.Name, properties.Driver.Name())
+
+	c := config.NewEmpty("test")
+	is.False(c.HasDecoder(properties.Name))
+	c.AddDriver(properties.Driver)
+
+	is.True(c.HasDecoder(properties.Name))
+	is.True(c.HasEncoder(properties.Name))
+
+	// 	m := struct {
+	// 		N string
+	// 	}{}
+	// 	err := properties.Decoder([]byte(`{
+	// // comments
+	// "n":"v"}
+	// `), &m)
+	// 	is.Nil(err)
+	// 	is.Eq("v", m.N)
+}
