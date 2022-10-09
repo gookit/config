3e8aeb50237528e1c56428b1a0644d9ea205fb7d
Inhere <in.798@qq.com>
2022-10-09 13:18:20 +0800

    chore: fix there are examples run error on _examples/

--- _examples/ini.go
+++ _examples/ini.go
@@ -33,24 +33,24 @@ func main() {
 	fmt.Printf("config data: \n %#v\n", config.Data())
 	fmt.Print("get config example:\n")
 
-	name, ok := config.String("name")
-	fmt.Printf("- get string\n ok: %v, val: %v\n", ok, name)
+	name := config.String("name")
+	fmt.Printf("- get string\n val: %v\n", name)
 
 	// NOTICE: ini is not support array
 
-	map1, ok := config.StringMap("map1")
-	fmt.Printf("- get map\n ok: %v, val: %#v\n", ok, map1)
+	map1 := config.StringMap("map1")
+	fmt.Printf("- get map\n val: %#v\n", map1)
 
-	val0, ok := config.String("map1.key")
-	fmt.Printf("- get sub-value by path 'map.key'\n ok: %v, val: %v\n", ok, val0)
+	val0 := config.String("map1.key")
+	fmt.Printf("- get sub-value by path 'map.key'\n val: %v\n", val0)
 
 	// can parse env name(ParseEnv: true)
-	fmt.Printf("get env 'envKey' val: %s\n", config.DefString("envKey", ""))
-	fmt.Printf("get env 'envKey1' val: %s\n", config.DefString("envKey1", ""))
+	fmt.Printf("get env 'envKey' val: %s\n", config.String("envKey", ""))
+	fmt.Printf("get env 'envKey1' val: %s\n", config.String("envKey1", ""))
 
 	// set value
-	config.Set("name", "new name")
-	name, ok = config.String("name")
-	fmt.Printf("- set string\n ok: %v, val: %v\n", ok, name)
+	_ = config.Set("name", "new name")
+	name = config.String("name")
+	fmt.Printf("- set string\n val: %v\n", name)
 
 }
--- _examples/toml.go
+++ _examples/toml.go
@@ -30,23 +30,23 @@ func main() {
 	fmt.Printf("config data: \n %#v\n", config.Data())
 	fmt.Print("get config example:\n")
 
-	name, ok := config.String("name")
-	fmt.Printf("- get string\n ok: %v, val: %v\n", ok, name)
+	name := config.String("name")
+	fmt.Printf("- get string\n val: %v\n", name)
 
-	arr1, ok := config.Strings("arr1")
-	fmt.Printf("- get array\n ok: %v, val: %#v\n", ok, arr1)
+	arr1 := config.Strings("arr1")
+	fmt.Printf("- get array\n val: %#v\n", arr1)
 
-	val0, ok := config.String("arr1.0")
-	fmt.Printf("- get sub-value by path 'arr.index'\n ok: %v, val: %v\n", ok, val0)
+	val0 := config.String("arr1.0")
+	fmt.Printf("- get sub-value by path 'arr.index'\n val: %v\n", val0)
 
-	map1, ok := config.StringMap("map1")
-	fmt.Printf("- get map\n ok: %v, val: %#v\n", ok, map1)
+	map1 := config.StringMap("map1")
+	fmt.Printf("- get map\n val: %#v\n", map1)
 
-	val0, ok = config.String("map1.name")
-	fmt.Printf("- get sub-value by path 'map.key'\n ok: %v, val: %v\n", ok, val0)
+	val0 = config.String("map1.name")
+	fmt.Printf("- get sub-value by path 'map.key'\n val: %v\n", val0)
 
 	// can parse env name(ParseEnv: true)
-	fmt.Printf("get env 'envKey' val: %s\n", config.DefString("envKey", ""))
-	fmt.Printf("get env 'envKey1' val: %s\n", config.DefString("envKey1", ""))
+	fmt.Printf("get env 'envKey' val: %s\n", config.String("envKey", ""))
+	fmt.Printf("get env 'envKey1' val: %s\n", config.String("envKey1", ""))
 
 }
